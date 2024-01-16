d52d9ebf0ed43082c57b46bdd33b12836984c8a7
Sophie Aubert <sophie.aubert@heig-vd.ch>
2024-01-16 14:16:24 +0100

    Ajout environments

--- src/app/layout/accueil/accueil.page.ts
+++ src/app/layout/accueil/accueil.page.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, ViewWillEnter } from '@ionic/angular';
 import { Router } from "@angular/router";
 import { AuthService } from "src/app/security/auth.service";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-accueil',
@@ -12,21 +13,29 @@ import { AuthService } from "src/app/security/auth.service";
   standalone: true,
   imports: [IonicModule, CommonModule, FormsModule]
 })
-export class AccueilPage implements OnInit {
 
+export class AccueilPage implements OnInit {
 constructor(
-  // Inject the authentication provider.
   private auth: AuthService,
-  // Inject the router
   private router: Router
   ) {}
-
   ngOnInit() {
   }
-
   logOut() {
     console.log("logging out...");
     this.auth.logOut();
     this.router.navigateByUrl("/login");
     }
+}
+
+export class accueilListePage implements ViewWillEnter {
+  constructor(private readonly http: HttpClient) {}
+
+  ionViewWillEnter(): void {
+    // Make an HTTP request to retrieve the trips.
+    const url = "https://thenicheapp.onrender.com/annonces";
+    this.http.get(url).subscribe((annonces) => {
+      console.log('Annonces chargées :', annonces);
+    });
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors} from '@angular/common/http';
 import { importProvidersFrom } from "@angular/core";
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 import { IonicStorageModule } from "@ionic/storage-angular";
+import { authInterceptor } from './app/security/auth.interceptor';
 
 if (environment.production) {
   enableProdMode();
@@ -21,5 +22,6 @@ bootstrapApplication(AppComponent, {
     provideRouter(routes),
     provideHttpClient(), 
     importProvidersFrom(IonicStorageModule.forRoot()),
+    provideHttpClient(withInterceptors([authInterceptor])),
   ],
 });
