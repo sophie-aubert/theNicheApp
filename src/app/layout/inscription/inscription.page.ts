3f8533806c7634ff1cc6b41f35e93a5df16a4594
Sophie Aubert <sophie.aubert@heig-vd.ch>
2024-01-16 15:42:48 +0100

    Inscription étape 1.1

--- src/app/layout/inscription/inscription.page.ts
+++ src/app/layout/inscription/inscription.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -8,13 +8,22 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: './inscription.page.html',
   styleUrls: ['./inscription.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule],
 })
 export class InscriptionPage implements OnInit {
+  inscriptionRequest: any = {}; // Un objet pour stocker les données du formulaire
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
+  onSubmit(form: NgForm) {
+    // Ne rien faire si le formulaire est invalide.
+    if (form.invalid) {
+      return;
+    }
+
+    // Vous pouvez traiter ici les données du formulaire (inscriptionRequest).
+    console.log('Formulaire soumis :', this.inscriptionRequest);
+  }
 }
