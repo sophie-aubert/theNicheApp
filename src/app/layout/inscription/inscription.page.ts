68d441d7fd79c3ca5ab38233be61a7b0c232cc15
Sophie Aubert <sophie.aubert@heig-vd.ch>
2024-01-22 09:39:28 +0100

    Page d'inscription - OK

--- src/app/layout/inscription/inscription.page.ts
+++ src/app/layout/inscription/inscription.page.ts
@@ -1,29 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule, NgForm } from '@angular/forms';
+// Assurez-vous d'importer le HttpClient dans votre composant
+import { HttpClient } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
+import { Component } from '@angular/core';
+import { AuthRequest } from 'src/app/security/auth-request.model';
 import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
-  selector: 'app-inscription',
-  templateUrl: './inscription.page.html',
-  styleUrls: ['./inscription.page.scss'],
+  selector: "app-login",
+  templateUrl: "./inscription.page.html",
+  styleUrls: ["./inscription.page.scss"],
   standalone: true,
   imports: [IonicModule, CommonModule, FormsModule],
 })
-export class InscriptionPage implements OnInit {
-  inscriptionRequest: any = {}; // Un objet pour stocker les données du formulaire
+export class InscriptionPage {
+  inscriptionRequest: AuthRequest = {};
 
-  constructor() {}
-
-  ngOnInit() {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   onSubmit(form: NgForm) {
-    // Ne rien faire si le formulaire est invalide.
     if (form.invalid) {
       return;
     }
 
-    // Vous pouvez traiter ici les données du formulaire (inscriptionRequest).
-    console.log('Formulaire soumis :', this.inscriptionRequest);
-  }
+    const apiUrl = 'https://thenicheapp.onrender.com';
+    const endpoint = `${apiUrl}/auth/inscription`;
+
+    this.http.post(endpoint, this.inscriptionRequest).subscribe(
+      (response: any) => {
+        console.log('Inscription réussie!', response);
+        // Rediriger l'utilisateur vers la page de connexion.
+        this.router.navigateByUrl('/accueil');
+    
+      },
+      (error: any) => {
+        console.error('Erreur lors de l\'inscription', error);
+
+        // Gérer les erreurs d'inscription ici.
+        if (error.status === 409) {
+          // Exemple: Afficher un message d'erreur pour un conflit (par exemple, utilisateur existant)
+          console.error('Utilisateur déjà existant');
+        } else {
+          // Gérer d'autres erreurs
+        }
+      }
+    );
+  }
 }
