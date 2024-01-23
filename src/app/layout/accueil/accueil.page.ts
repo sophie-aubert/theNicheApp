9bc0bc0f31dd03573b1fb927abcfa29ba4b718b3
Ferida Papracanin <ferida.papracanin@heig-vd.ch>
2024-01-23 13:07:17 +0100

    geoloc

--- src/app/layout/accueil/accueil.page.ts
+++ src/app/layout/accueil/accueil.page.ts
@@ -2,39 +2,74 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule, ViewWillEnter } from '@ionic/angular';
-import { Router, RouterModule } from "@angular/router";
-import { AuthService } from "src/app/security/auth.service";
-import { HttpClient } from "@angular/common/http";
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from 'src/app/security/auth.service';
+import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Geolocation } from '@capacitor/geolocation';
 import { ToastController } from '@ionic/angular';
 import { IonModal } from '@ionic/angular';
 import { ViewChild } from '@angular/core';
 import { OverlayEventDetail } from '@ionic/core/components';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { latLng, MapOptions, tileLayer } from 'leaflet';
+import { Map } from 'leaflet';
+import { defaultIcon } from './default-marker';
+import { marker, Marker } from 'leaflet';
 
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.page.html',
   styleUrls: ['./accueil.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, RouterModule]
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    LeafletModule,
+  ],
 })
-
 export class AccueilPage implements OnInit, ViewWillEnter {
-  annonces: any[] = []; 
+  annonces: any[] = [];
   annonceSelectionnee: any; // Nouvelle propriété
-
+  mapOptions: MapOptions;
+  mapMarkers: Marker[];
 
   constructor(
     private auth: AuthService,
     private router: Router,
     private readonly http: HttpClient,
     private toastController: ToastController
-  )  {
+  ) {
     // Initialisation de 'modal'
     this.modal = {} as IonModal;
     // Initialisation de 'name'
     this.name = '';
+
+    this.mapOptions = {
+      layers: [
+        tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          maxZoom: 18,
+        }),
+      ],
+      zoom: 13,
+      center: latLng(46.778186, 6.641524),
+    };
+
+    this.mapMarkers = [
+      marker([46.778186, 6.641524], { icon: defaultIcon }),
+      marker([46.780796, 6.647395], { icon: defaultIcon }),
+      marker([46.784992, 6.652267], { icon: defaultIcon }),
+    ];
+  }
+
+  onMapReady(map: Map) {
+    console.log('Map is ready');
+    setTimeout(() => {
+      map.invalidateSize();
+      console.log('Map should resize');
+    }, 200);
   }
 
   ngOnInit() {
@@ -63,56 +98,50 @@ export class AccueilPage implements OnInit, ViewWillEnter {
   }
 
   logOut() {
-    console.log("logging out...");
+    console.log('logging out...');
     this.auth.logOut();
-    this.router.navigateByUrl("/login");
+    this.router.navigateByUrl('/login');
   }
 
   // Nouvelle méthode pour gérer le clic sur le bouton "Panier"
   panier() {
-    this.router.navigateByUrl("/panier");
+    this.router.navigateByUrl('/panier');
   }
   profil() {
-    this.router.navigateByUrl("/donnees-perso");
+    this.router.navigateByUrl('/donnees-perso');
   }
 
-
-   // Nouvelle méthode pour gérer le clic sur une annonce
-   onAnnonceClick(annonce: any) {
+  // Nouvelle méthode pour gérer le clic sur une annonce
+  onAnnonceClick(annonce: any) {
     console.log('Annonce cliquée :', annonce);
     this.annonceSelectionnee = annonce;
     this.router.navigate(['/article', this.annonceSelectionnee.id]); // Passer l'ID lors de la navigation
   }
 
-////////////////////////////AFFICHER MODAL//////////////////////////////////////
+  ////////////////////////////AFFICHER MODAL//////////////////////////////////////
 
-@ViewChild(IonModal) modal: IonModal;
+  @ViewChild(IonModal) modal: IonModal;
 
+  message =
+    'This modal example uses triggers to automatically open a modal when the button is clicked.';
+  name: string;
 
-message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
-name : string;
-
-cancel() {
-  this.modal.dismiss(null, 'cancel');
-}
-
-confirm() {
-  this.modal.dismiss(this.name, 'confirm');
-}
-
-onWillDismiss(event: Event) {
-  const ev = event as CustomEvent<OverlayEventDetail<string>>;
-  if (ev.detail.role === 'confirm') {
-  this.message = `Hello, ${ev.detail.data}!`;
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
   }
-}
 
+  confirm() {
+    this.modal.dismiss(this.name, 'confirm');
+  }
 
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.message = `Hello, ${ev.detail.data}!`;
+    }
+  }
 
-
-
-
-//////////////////////////////GEOLOCALISATION//////////////////////////////////////
+  //////////////////////////////GEOLOCALISATION//////////////////////////////////////
   async getCurrentLocation() {
     const coordinates = await Geolocation.getCurrentPosition();
     return coordinates.coords;
@@ -129,11 +158,14 @@ onWillDismiss(event: Event) {
       // Affichez un message à l'utilisateur
       const toast = await this.toastController.create({
         message: 'Carte affichée avec les annonces et distances',
-        duration: 2000
+        duration: 2000,
       });
       toast.present();
     } catch (error) {
-      console.error('Erreur lors de la récupération de la position actuelle', error);
+      console.error(
+        'Erreur lors de la récupération de la position actuelle',
+        error
+      );
     }
   }
 
@@ -141,7 +173,7 @@ onWillDismiss(event: Event) {
     // Implémentez la logique pour calculer la distance pour chaque annonce
     // Vous pouvez utiliser la formule Haversine ou d'autres méthodes de calcul de distance
     // Ensuite, ajoutez la distance aux données de l'annonce
-    const annoncesAvecDistance = this.annonces.map(annonce => {
+    const annoncesAvecDistance = this.annonces.map((annonce) => {
       const distance = this.calculerDistanceEntreDeuxPoints(
         location.latitude,
         location.longitude,
@@ -155,9 +187,14 @@ onWillDismiss(event: Event) {
     return annoncesAvecDistance;
   }
 
-  calculerDistanceEntreDeuxPoints(lat1: number, lon1: number, lat2: number, lon2: number) {
+  calculerDistanceEntreDeuxPoints(
+    lat1: number,
+    lon1: number,
+    lat2: number,
+    lon2: number
+  ) {
     // Implémentez la formule Haversine ou utilisez des bibliothèques comme haversine-distance
     // pour calculer la distance entre deux points géographiques
     // Retournez la distance calculée
-  }
+  }
 }
